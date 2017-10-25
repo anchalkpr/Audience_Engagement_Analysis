8f2706fec9e96d4826dc85190b25ab0c98cd99a5
Manal Gandhi <manalgan@usc.edu>
2017-10-25 01:26:52 -0700

    Added code to track and recognoze faces in a video

---
+++ src/face_recog_track.py
@@ -0,0 +1,122 @@
+## Detects the faces in the first frame
+## and stores the cropped image for these faces in the output directory for the subsequent frames
+
+import face_recognition
+import cv2
+import os, shutil
+import time
+
+####### CONSTANTS #################
+path_output_dir = os.path.join("..", "output")
+UNKNOWN = "unknown"
+DEBUG = True
+###################################
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+def clean_dir(path):
+    for the_file in os.listdir(path):
+        file_path = os.path.join(path, the_file)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+
+# Cleanup output directory
+clean_dir(path_output_dir)
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+input("Press Enter when you are ready")
+ret, init_frame = video_capture.read()
+
+init_frame_face_locations = face_recognition.face_locations(init_frame)
+faceid_list = list()
+face_encodings_list = list()
+
+init_file_output_dir = os.path.join(path_output_dir, "init")
+os.makedirs(init_file_output_dir)
+#store faces in a file, find encodings
+for i in range(len(init_frame_face_locations)):
+    face_location = init_frame_face_locations[i]
+    top, right, bottom, left = face_location
+
+    face_image = init_frame[top:bottom, left:right]
+    face_id = "face_" + str(i)
+    path_file = os.path.join(init_file_output_dir, face_id + ".png")
+
+    cv2.imwrite(path_file, face_image)
+    face_encodings_list.append(face_recognition.face_encodings(face_image)[0])
+    faceid_list.append(face_id)
+
+process_this_frame = True
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(small_frame)
+        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+
+        face_name_list = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            match_list = face_recognition.compare_faces(face_encodings_list, face_encoding)
+            name = UNKNOWN
+
+            for i in range(len(match_list)):
+                if match_list[i] and faceid_list[i] not in face_name_list:
+                    name = faceid_list[i]
+                    break
+
+            face_name_list.append(name)
+
+        # store all the faces in this directory
+        current_output_dir = os.path.join(path_output_dir, str(current_milli_time()))
+        os.makedirs(current_output_dir)
+
+    process_this_frame = not process_this_frame
+
+    unknown_counter = 0
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_name_list):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4; right *= 4; bottom *= 4; left *= 4
+        if name is UNKNOWN:
+            name = name + "_" +str(unknown_counter)
+            unknown_counter+=1
+        face_image = frame[top:bottom, left:right]
+        path_file = os.path.join(current_output_dir, name + ".png")
+        cv2.imwrite(path_file, face_image)
+
+        if DEBUG:
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    if DEBUG:
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+# Cleanup output directory
+if not DEBUG:
+    clean_dir(path_output_dir)
